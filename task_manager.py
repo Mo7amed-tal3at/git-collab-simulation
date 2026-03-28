6f6b80acc91c5f435b86db7d5fec13a290299e8f
mtalaat <mo7amedtal3at21@gmail.com>
2026-03-28 19:16:05 +0200

    feat: add task function

---
+++ task_manager.py
@@ -0,0 +1,13 @@
+# Task Manager - Main File
+tasks = []
+
+def show_tasks():
+    if not tasks:
+        print("No tasks yet!")
+        return
+    for i, task in enumerate(tasks, 1):
+        print(f"{i}. {task}")
+
+def add_task(task):
+    tasks.append(task)
+    print(f"Task '{task}' added!")
